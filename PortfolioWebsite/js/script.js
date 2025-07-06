0608d1ff8d4e39f416d4a6a1196c15aee964e577
Deniz <167799743+T-vulpes@users.noreply.github.com>
2025-07-06 15:00:02 +0300

    0607

--- PortfolioWebsite/js/script.js
+++ PortfolioWebsite/js/script.js
@@ -4,12 +4,6 @@ document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
 }
 
-// searchForm ve cartItem ile ilgili diğer kodları kaldırın
-// let searchForm = document.querySelector('.search-form');
-// document.querySelector('#search-btn').onclick = () =>{...}
-// let cartItem = document.querySelector('.cart-items-container');
-// document.querySelector('#cart-btn').onclick = () =>{...}
-
 window.onscroll = () =>{
     navbar.classList.remove('active');
     // searchForm.classList.remove('active'); // Kaldırıldı
