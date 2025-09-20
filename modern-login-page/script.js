92c9d7d6f82482bca1ade3beed12aced51dac04c
Deniz <167799743+T-vulpes@users.noreply.github.com>
2025-09-20 14:53:59 +0300

    login-page

---
+++ modern-login-page/script.js
@@ -0,0 +1,24 @@
+// Videonun oynatma hızını ayarlar
+document.addEventListener('DOMContentLoaded', function() {
+    const video = document.getElementById('background-video');
+    if (video) {
+        video.playbackRate = 5; // 1.2 kat daha hızlı oynat
+    }
+});
+
+// Şifrenin görünürlüğünü değiştiren fonksiyon
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById('password');
+    const toggleIcon = document.getElementById('toggle-icon');
+
+    // Eğer şifre alanı 'password' tipindeyse
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text'; // Tipi 'text' yaparak şifreyi göster
+        toggleIcon.classList.remove('fa-eye');
+        toggleIcon.classList.add('fa-eye-slash'); // Gözü kapalı ikonuna dönüştür
+    } else {
+        passwordInput.type = 'password'; // Tipi 'password' yaparak şifreyi gizle
+        toggleIcon.classList.remove('fa-eye-slash');
+        toggleIcon.classList.add('fa-eye'); // Gözü açık ikonuna dönüştür
+    }
+}
