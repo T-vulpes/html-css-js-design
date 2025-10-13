94c26498973bf4d292cd6b1f78ad99dae5d8bbd4
Deniz <167799743+T-vulpes@users.noreply.github.com>
2025-10-13 15:28:14 +0300

    bgnr

--- preloader animation/script.js
+++ preloader animation/script.js
@@ -7,18 +7,12 @@
             
             const ANIMATION_DURATION = 4000; 
             const counterInterval = ANIMATION_DURATION / 100;
-            
             let currentPercentage = 0;
 
             const interval = setInterval(() => {
                 if (currentPercentage < 100) {
-                    currentPercentage++;
-                    
-                    // 1. Sayacı güncelle
+                    currentPercentage++;                    
                     percentageCounter.textContent = currentPercentage + '%';
-
-                    // 2. KİLİT NOKTA 3: CSS'teki değişkeni JavaScript ile anlık olarak güncelle
-                    // Bu satır, JavaScript ve CSS arasındaki sihirli bağlantıdır.
                     loaderLogo.style.setProperty('--fill-percentage', currentPercentage + '%');
                 
                 } else {
@@ -36,3 +30,4 @@
 
 
         /* ===== JAVASCRIPT KODLARI BİTİŞİ ===== */
+
