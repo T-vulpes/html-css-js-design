d26770ca038fa7789df0dac5560235e54cb3d7cf
Deniz <167799743+T-vulpes@users.noreply.github.com>
2025-10-22 13:18:01 +0300

    loader design

--- Pulsing Ripple Loader/script.js
+++ Pulsing Ripple Loader/script.js
@@ -1,11 +1,7 @@
 window.addEventListener('load', function() {
     const loaderContainer = document.getElementById('loader-container');
     const body = document.body;
-
-    // Loader'ı gizlemek için 'hidden' sınıfını ekle
     loaderContainer.classList.add('hidden');
-    
-    // Loader gizlendikten sonra sayfanın tekrar kaydırılabilmesini sağla
     body.style.overflow = 'auto'; 
-
 });
+
