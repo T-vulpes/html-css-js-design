cab50af7c6ff060aca3bf903fa9431557ae53a6b
Deniz <167799743+T-vulpes@users.noreply.github.com>
2026-07-15 17:59:14 +0300

    beginner-friendly

--- animated-accordion-gallery/script.js
+++ animated-accordion-gallery/script.js
@@ -1,19 +1,11 @@
-// Tüm panelleri seç
 const panels = document.querySelectorAll('.panel');
-
-// Her bir panele tıklama özelliği ekle
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
-        
-        // Önce tüm panellerden 'active' sınıfını temizle (Küçülsünler)
         removeActiveClasses();
-        
-        // Sadece tıklanan panele 'active' sınıfını ekle (Büyüsün)
         panel.classList.add('active');
     });
 });
 
-// Temizleyici fonksiyon
 function removeActiveClasses() {
     panels.forEach(panel => {
         panel.classList.remove('active');
